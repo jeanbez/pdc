2c8087a781399e81f6adea4a012a34d5081a3b88
Jean Luca Bez <jeanlucabez@gmail.com>
2026-01-20 13:13:29 -0800

    include libtool

--- .github/workflows/dependencies-macos.sh
+++ .github/workflows/dependencies-macos.sh
@@ -4,7 +4,7 @@ set -eu -o pipefail
 
 export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=FALSE
 
-brew install open-mpi automake
+brew install open-mpi libtool automake
 
 # libfabric
 wget https://github.com/ofiwg/libfabric/archive/refs/tags/v1.15.2.tar.gz
